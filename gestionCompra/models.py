72553db123a6f812700a2fc54d96c3e6f4d7d050
silvana <silvanae16@gmail.com>
2021-11-02 19:58:55 -0300

    relacion_categoria

--- gestionCompra/models.py
+++ gestionCompra/models.py
@@ -13,6 +13,17 @@ class Cliente(models.Model):
     def __str__(self):
         return self.nombre
 
+class Categoria(models.Model):
+    nombre = models.CharField(max_length=30)
+
+    class Meta:
+        verbose_name='Categoria'
+        verbose_name_plural='Categorias'
+        db_table='gestionCompra_categoria'
+
+    def __str__(self):
+        return self.nombre
+
 
 class Producto(models.Model):
     nombre = models.CharField(max_length=50)
@@ -20,6 +31,7 @@ class Producto(models.Model):
     editorial = models.CharField(max_length=30)
     precio = models.IntegerField()
     imagen = models.ImageField(upload_to='media')
+    categoria=models.ForeignKey(Categoria, null=True, blank=True, on_delete=models.RESTRICT)
 
     def __str__(self):
         return "El nombre es %s el autor es %s la editorial es %s y el precio es %s" %(self.nombre, self.autor, self.editorial, self.precio)
@@ -30,8 +42,7 @@ class Compra(models.Model):
     fecha = models.DateField()
     total = models.IntegerField()
 
-class Categoria(models.Model):
-    nombre = models.CharField(max_length=30)
+
 
 class Administrador(models.Model):
     email = models.EmailField()
